93052925e0e4bf6d1706361a6e2fba205bcdb51f
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2013-03-03 16:20:46 -0600

    Added index creation instructions.

--- src/main/resources/db_scripts/schema.sql
+++ src/main/resources/db_scripts/schema.sql
@@ -2,4 +2,13 @@ create database provenance;
 
 create table provenance_data (resource VARCHAR(100) CHARACTER SET utf8, accessor VARCHAR(200) CHARACTER SET utf8, access_timestamp TIMESTAMP, operation VARCHAR(10) CHARACTER SET UTF8, data VARCHAR(1024) CHARACTER SET UTF8);
 
+create index resource_idx USING HASH on provenance_data (resource);
+
+create index accessor_idx USING HASH on provenance_data (accessor);
+
+create index timestamp_idx USING HASH on provenance_data (access_timestamp);
+
+create index operation_idx USING HASH on provenance_data (operation);
+
+
 
